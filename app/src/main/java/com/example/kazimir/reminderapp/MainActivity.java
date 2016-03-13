5567e15d7234d62ccdcbf04181db49f9959ea6f5
Kazimir Kovalenko <mirkovalenko@yandex.ru>
2016-03-13 12:46:07 +0300

    Buttons added, Listeners added

--- app/src/main/java/com/example/kazimir/reminderapp/MainActivity.java
+++ app/src/main/java/com/example/kazimir/reminderapp/MainActivity.java
@@ -1,46 +1,78 @@
 package com.example.kazimir.reminderapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.kazimir.reminderapp.dbl.TasksDatabase;
 import com.example.kazimir.reminderapp.model.Task;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    private final String SELECTED_TASK_KEY = "selectedTaskKey";
-    private final int REQUEST_CODE = 1;
+import static android.graphics.Color.GREEN;
 
-    private TasksDatabase tasksDatabase;
+public class MainActivity extends AppCompatActivity {
+    public static final String SELECTED_TASK_KEY = "selectedTaskKey";
+    public static final int REQUEST_CODE = 1;
+
+    public TasksDatabase tasksDatabase;
     private List<Task> tasks;
     private ListView tasksListView;
+
+    public int listItemPosition;
+    View selectedItem;
+    TaskAdapter taskAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tasksListView = (ListView) findViewById(R.id.catsListView);
+        tasksListView = (ListView) findViewById(R.id.tasksListView);
 
         tasksDatabase = new TasksDatabase();
         tasks = tasksDatabase.getCats();
 
-        TaskAdapter taskAdapter = new TaskAdapter(this, R.layout.task_template, tasks);
+        taskAdapter = new TaskAdapter(this, R.layout.task_template, tasks);
 
-        tasksListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
+
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                listItemPosition = position;
+                selectedItem = view;
                 Intent intent = new Intent(MainActivity.this, SelectedTaskActivity.class);
-                intent.putExtra(SELECTED_TASK_KEY, tasks.get(position).getTask().toString());
+                intent.putExtra(SELECTED_TASK_KEY, tasks.get(listItemPosition).getTask());
                 startActivityForResult(intent, REQUEST_CODE);
             }
-        });
+        };
 
+        tasksListView.setOnItemClickListener(onItemClickListener);
         tasksListView.setAdapter(taskAdapter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == SelectedTaskActivity.TASK_DONE_RESULT) {
+            if (requestCode == REQUEST_CODE) {
+                Toast.makeText(this, "Attempt to done item", Toast.LENGTH_SHORT).show();
+                taskAdapter.get
+            }
+        }
+        if (resultCode == SelectedTaskActivity.REMOVE_TASK_RESULT) {
+            if (requestCode == REQUEST_CODE) {
+                Toast.makeText(this, "Attempt to remove item", Toast.LENGTH_SHORT).show();
+            }
+        }
+        if (resultCode == SelectedTaskActivity.TASK_CHANGED_RESULT) {
+            if (requestCode == REQUEST_CODE) {
+                Toast.makeText(this, "Attempt to change item", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/kazimir/reminderapp/SelectedTaskActivity.java
+++ app/src/main/java/com/example/kazimir/reminderapp/SelectedTaskActivity.java
@@ -1,13 +1,66 @@
 package com.example.kazimir.reminderapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SelectedTaskActivity extends AppCompatActivity {
+    public static final String RESPONSE_KEY = "backIntentValue";
+    public static final int TASK_CHANGED_RESULT = 1;
+    public static final int REMOVE_TASK_RESULT = 2;
+    public static final int TASK_DONE_RESULT = 3;
+
+    TextView selectedTaskTextView;
+    EditText selectedActivityEditText;
+    String selectedTask;
+    Button removeButton;
+    Button doneButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selected_task);
+
+        selectedTaskTextView = (TextView) findViewById(R.id.selectedTaskTextView);
+        selectedActivityEditText = (EditText) findViewById(R.id.selectedTaskEditText);
+        removeButton = (Button) findViewById(R.id.removeButton);
+        doneButton = (Button) findViewById(R.id.doneButton);
+
+        selectedTask = getIntent().getExtras().getString(MainActivity.SELECTED_TASK_KEY);
+
+        selectedActivityEditText.setText(getIntent().getExtras().getString(MainActivity.SELECTED_TASK_KEY));
+
+        removeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(REMOVE_TASK_RESULT);
+                finish();
+            }
+        });
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(TASK_DONE_RESULT);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent backIntent = new Intent();
+        if (!selectedActivityEditText.getText().toString().equals(selectedTask)) {
+            backIntent.putExtra(RESPONSE_KEY, selectedActivityEditText.getText().toString());
+            setResult(TASK_CHANGED_RESULT, backIntent);
+        }
+        super.onBackPressed();
     }
 }
